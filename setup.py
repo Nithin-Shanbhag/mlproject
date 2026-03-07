d7781c738d95f3715e5f7bf0f812759ae748a4e5
Nithin Shanbhag <nnithinshanbhag@gmail.com>
2026-03-07 14:05:49 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import find_packages, setup
+from typing import List
+
+HYPEN_E_DOT='-e .'
+
+def get_requirements(file_path:str)->List[str]:
+    '''
+    This function will return the list of requirements
+    '''
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        ## removing the new line character from the requirements
+        requirements=[req.replace("\n","") for req in requirements]
+        
+        ## remoing the -e . from the requirements
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+
+
+setup(
+    name='mlproject',
+    version='0.0.1',
+    author='Nithin Shanbhag',
+    author_email='nnithinshanbhag@gmail.com',
+    packages=find_packages(),
+    ## install_requires=['pandas','numpy','seaborn']
+    install_requires=get_requirements('requirements.txt')
+    
+    
+)
